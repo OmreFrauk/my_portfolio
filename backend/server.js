ac2be71f9b55959d00bf69df89206c4c7f35f456
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2025-01-01 16:07:55 +0300

    changed cors options

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.REACT_APP_API_BASE,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
