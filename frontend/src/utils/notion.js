b91306d81d3e71d5abb5d91ef24214865ab4d34d
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2025-01-01 16:36:26 +0300

    bugfix

--- frontend/src/utils/notion.js
+++ frontend/src/utils/notion.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = "backend-five-rho-75.vercel.app";
 
 export const getProjects = async () => {
   try {
