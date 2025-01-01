ffda1193266062ed14ea0d7e649f4942132bec03
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2025-01-01 15:51:51 +0300

    prettiefier ui

--- frontend/src/components/WorksFrame.js
+++ frontend/src/components/WorksFrame.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles.css";
-import bio from "../assets/image.png";
+
 export const WorkFrame = ({
   project: {
     project_name,
@@ -12,89 +12,63 @@ export const WorkFrame = ({
   },
 }) => {
   const colorMap = {
-    React: "#28456c",
-    Javascript: "#493064",
-    Python: "#3776ab",
-    "Amazon Services": "#603b2d",
-    Mongoose: "#69314c",
+    React: "#61dafb33",
+    Javascript: "#f7df1e33",
+    Python: "#3776ab33",
+    "Amazon Services": "#ff990033",
+    Mongoose: "#88000033",
+    // Diğer teknolojiler için renkler ekleyebilirsiniz
   };
-  console.log("github", project_url.url);
+
   return (
     <div className="workFrame">
-      {/* Image Section */}
       <div className="work-photo">
         <img
           className="portrait"
-          alt="Portrait"
+          alt={project_name.title[0].plain_text}
           src={project_screenshots.files[0].file.url}
+          loading="lazy"
         />
       </div>
 
-      {/* Content Section */}
       <div className="work-content">
-        {/* Title */}
-        <div className="work-title">{project_name.title[0].plain_text}</div>
-
-        {/* Description */}
-        <div className="work-description">
+        <h2 className="work-title">{project_name.title[0].plain_text}</h2>
+        <p className="work-description">
           {project_description.rich_text[0].plain_text}
-        </div>
+        </p>
 
-        {/* Project Info */}
         <div className="work-info">
           <div>
             <span>Year</span>
-            <span>2022</span>
+            <span>{project_year || "2022"}</span>
           </div>
           <div className="tech">
             <span>Technologies</span>
-            <div className="tech-tags">
-              {technologies.multi_select.map((techItem) => {
-                const tagColor = colorMap[techItem.name] || "#333";
-                return (
-                  <span
-                    key={techItem.id}
-                    className="tech-tag"
-                    style={{ backgroundColor: tagColor }}
-                  >
-                    {techItem.name}
-                  </span>
-                );
-              })}
-            </div>
+            <span className="tech-tags">
+              {technologies.multi_select.map((tech) => (
+                <span
+                  key={tech.id}
+                  className="tech-tag"
+                  style={{ backgroundColor: colorMap[tech.name] || "#33333333" }}
+                >
+                  {tech.name}
+                </span>
+              ))}
+            </span>
           </div>
         </div>
 
-        {/* Links */}
         <div className="work-links">
-          <div className="link-div">
-            <div className="link">
-              <a href="#live-demo" target="_blank" rel="noopener noreferrer">
-                LIVE DEMO
-              </a>
-              <img src={require("../assets/ra.png")} alt="arrow" />
+          {project_url?.url && (
+            <div className="link-div">
+              <div className="link">
+                <a href={project_url.url} target="_blank" rel="noopener noreferrer">
+                  SEE ON GITHUB
+                  <img src={require("../assets/github.png")} alt="github" width="20" height="20" />
+                </a>
+              </div>
             </div>
-            <div
-              className="line"
-              style={{ background: "#d3e879", opacity: "100%" }}
-            ></div>
-          </div>
-          <div className="link-div">
-            <div className="link">
-              <a
-                href={project_url.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                SEE ON GITHUB
-              </a>
-              <img src={require("../assets/github.png")} alt="github" />
-            </div>
-            <div
-              className="line"
-              style={{ background: "#d3e879", opacity: "100%" }}
-            ></div>
-          </div>
+          )}
         </div>
       </div>
     </div>
